4a8aae6967ac77ad4af2f1cb8048375277d673f4
JangWonJong <jwj9922@naver.com>
2022-06-10 16:08:13 +0900

    data CRUD

--- crime/crime.py
+++ crime/crime.py
@@ -1,7 +1,5 @@
-
-
 from context.domains import Reader, Printer, File
-
+import folium
 
 def test():
     pass
@@ -31,15 +29,54 @@ class Solution(Reader):
         station_names = []
         for name in crime['관서명']:
             station_names.append(f'서울{str(name[:-1])}경찰서')
+        print(f'station_names range: {len(station_names)}')
+        for i, name in enumerate(station_names):
+            print(f'name {i} {name}')
+        gmaps = self.gmaps()
+        a = gmaps.geocode('서울종암경찰서', language='ko')
+        print(a)
+        '''
+        [{'address_components': [{'long_name': '２８３', 'short_name': '２８３', 'types': ['premise']},
+         {'long_name': '노원로', 'short_name': '노원로', 'types': ['political', 'sublocality', 'sublocality_level_4']},
+         {'long_name': '노원구', 'short_name': '노원구', 'types': ['political', 'sublocality', 'sublocality_level_1']},
+         {'long_name': '서울특별시', 'short_name': '서울특별시', 'types': ['administrative_area_level_1', 'political']},
+         {'long_name': '대한민국', 'short_name': 'KR', 'types': ['country', 'political']},
+         {'long_name': '139-230', 'short_name': '139-230', 'types': ['postal_code']}],
+          'formatted_address': '대한민국 서울특별시 노원구 노원로 283',
+          'geometry': {'location': {'lat': 37.6421389, 'lng': 127.0710473},
+          'location_type': 'ROOFTOP',
+          'viewport': {'northeast': {'lat': 37.6434878802915, 'lng': 127.0723962802915},
+          'southwest': {'lat': 37.6407899197085, 'lng': 127.0696983197085}}}, 
+          'partial_match': True, 'place_id': 'ChIJQ9zARmW5fDURgrNe_TNJWP4', 
+          'plus_code': {'compound_code': 'J3RC+VC 대한민국 서울특별시', 'global_code': '8Q99J3RC+VC'}, 
+          'types': ['establishment', 'point_of_interest', 'police']}]
+        '''
+
         station_addrs = []
         station_lats = []
-        station_lngs = []
-        gmaps = self.gmaps()
-        for name in station_names:
-            temp = gmaps
+        station_lng = []
+        for i, name in enumerate(station_names):
+            if name != '서울종암경찰서':
+                temp = gmaps.geocode(name, language= 'ko')
+            else:
+                temp = [{'address_components': [{'long_name': '2 3', 'short_name': '2 ３', 'types': ['premise']},
+                                                {'long_name': '서울', 'short_name': '종암로', 'types': ['political', 'sublocality', 'sublocality_level_4']},
+                                                {'long_name': '성북구', 'short_name': '성북구', 'types': ['political', 'sublocality', 'sublocality_level_1']},
+                                                {'long_name': '서울특별시', 'short_name': '서울특별시', 'types': ['administrative_area_level_1', 'political']},
+                                                {'long_name': '대한민국', 'short_name': 'KR', 'types': ['country', 'political']},
+                                                {'long_name': '139-230', 'short_name': '139-230', 'types': ['postal_code']}],
+                         'formatted_address': '서울 성북구 종암로23길 52',
+                         'geometry': {'location': {'lat': 37.60388169879458, 'lng': 127.04001571848704},
+                                      'location_type': 'ROOFTOP',
+                                      'viewport': {'northeast': {'lat': 37.60388169879458, 'lng': 127.04001571848704},
+                                                   'southwest': {'lat': 37.60388169879458, 'lng': 127.04001571848704}}},
+                         'partial_match': True, 'place_id': 'ChIJQ9zARmW5fDURgrNe_TNJWP4',
+                         'plus_code': {'compound_code': 'J3RC+VC 대한민국 서울특별시', 'global_code': '8Q99J3RC+VC'},
+                         'types': ['establishment', 'point_of_interest', 'police']}]
+
+            print(f'name {i} = {temp[0].get("formatted_address")}')
 
     def save_cctv_pos(self):
-
         self.file.fname = 'cctv_in_seoul'
         print(self.csv(self.file))
         # 헤더는 2행, 사용하는 컬럼은 B, D, G, J, N 을 사용한다.
@@ -52,7 +89,16 @@ class Solution(Reader):
         pass
 
     def folium_test(self):
-        pass
+        self.file.fname = 'us-states'
+        states = self.json(self.file)
+        self.file.fname = 'us_unemployment'
+        unemployment = self.csv(self.file)
+
+        bins = list(states["Unemployment"].quantitle(0, 0.25, 0.5, 0.75, 1))
+
+        #m = folium.Map(location=[37.60388169879458, 127.04001571848704])
+        #m.save("./save/folium_test.html")
+
 
     def draw_crime_map(self):
         self.file.fname = 'geo_simple'
@@ -60,7 +106,7 @@ class Solution(Reader):
 
 if __name__ == '__main__':
     #Solution().save_police_pos()
-    #Solution().save_police_pos2('crime_in_seoul')
     #Solution().save_police_pos3()
-    Solution().save_cctv_pos()
+    #Solution().save_cctv_pos()
+    Solution().folium_test()
     #Solution().draw_crime_map()
