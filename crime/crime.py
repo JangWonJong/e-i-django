5ac3ab497a93e473c7eb5b878d76306b702d5318
JangWonJong <jwj9922@naver.com>
2022-06-13 18:33:36 +0900

    2022-06-13 18:33

--- crime/crime.py
+++ crime/crime.py
@@ -1,12 +1,11 @@
 import numpy as np
 from icecream import ic
 import pandas as pd
+from sklearn import preprocessing
 
-from context.domains import Reader, Printer, File
+from context.domains import Reader, File
 import folium
 
-def test():
-    pass
 
 class Solution(Reader):
 
@@ -24,6 +23,7 @@ class Solution(Reader):
             print('1. crime_in_seoul.csv, 구글맵 API 를 이용해서 서울시내 경찰서 주소목록파일을 작성하시오.')
             print('2. us-states.json, us_unemployment.csv 를 이용해서 미국 실업률 지도(folium_test.html)를 작성하시오.')
             print('3. cctv_in_seoul.csv, pop_in_seoul.csv 를 이용해서 서울시내 경찰서 주소목록파일(cctv_pop.csv)을 작성하시오.')
+            print('4. police_pos.csv, 를 이용해서 경찰서 범죄검거율 정규화파일(police_norm.csv)을 작성하시오.')
             return input('메뉴 선택 \n')
 
         while 1:
@@ -36,6 +36,8 @@ class Solution(Reader):
                 self.folium_test()
             if menu == '3':
                 self.save_cctv_pos()
+            if menu == '4':
+                self.save_police_norm()
             elif menu == '0':
                 break
 
@@ -58,9 +60,10 @@ class Solution(Reader):
         for i, name in enumerate(station_names):
             print(f'name {i} {name}')
         gmaps = self.gmaps()
+
+        '''
         a = gmaps.geocode('서울종암경찰서', language='ko')
         #print(a)
-        '''
         [{'address_components': [{'long_name': '２８３', 'short_name': '２８３', 'types': ['premise']},
          {'long_name': '노원로', 'short_name': '노원로', 'types': ['political', 'sublocality', 'sublocality_level_4']},
          {'long_name': '노원구', 'short_name': '노원구', 'types': ['political', 'sublocality', 'sublocality_level_1']},
@@ -115,8 +118,8 @@ class Solution(Reader):
             gu_name = [gu for gu in temp if gu[-1] == '구'][0]
             gu_names.append(gu_name)
         crime['구별'] = gu_names
-        print(gu_names)
-        crime.to_csv('./save/police_pos.csv')
+        ic(crime)
+        crime.to_csv('./save/police_pos.csv', index=False)
 
     def save_cctv_pos(self):
         self.file.fname = 'cctv_in_seoul'
@@ -198,8 +201,68 @@ class Solution(Reader):
 
         cctv_pop.to_csv('./save/cctv_pop.csv')
 
+        '''
+        표준화는 데이터의 평균을 0, 표준편차를 1로 만드는 것이다.
+        x = (x - mu) / sigma
+        scale = (x - np.mean(x, axis=0)) / np.std(x, axis=0)
+        정규화는 최대값을 1, 최솟값을 0으로 만드는 것이다.
+        '''
     def save_police_norm(self):
-        pass
+
+        file = self.file
+        file.context = './save/'
+        file.fname = 'police_pos'
+        police_pos = self.csv(file)
+        police = pd.pivot_table(police_pos, index='구별', aggfunc=np.sum)
+        police['살인검거율'] = police['살인 검거'] / police['살인 발생'] * 100
+        police['강도검거율'] = police['강도 검거'] / police['강도 발생'] * 100
+        police['강간검거율'] = police['강간 검거'] / police['강간 발생'] * 100
+        police['절도검거율'] = police['절도 검거'] / police['절도 발생'] * 100
+        police['폭력검거율'] = police['폭력 검거'] / police['폭력 발생'] * 100
+        police.drop(columns={police.columns[1],
+                             police.columns[3],
+                             police.columns[5],
+                             police.columns[7],
+                             police.columns[9]}, inplace=True)
+
+        for i in self.crime_rate_columns:
+            police[i].loc[police[i]> 100] = 100
+        police.to_csv('./save/police.csv', sep=',', encoding='UTF-8')
+        police.rename(columns={police.columns[1]:'살인',
+                               police.columns[2]:'강도',
+                               police.columns[3]:'강간',
+                               police.columns[4]:'절도',
+                               police.columns[5]:'폭력',}, inplace=True)
+        ic(police)
+
+        '''
+        x= police[self.crime_rate_columns].values
+        min_max_scalar = preprocessing.MinMaxScaler()
+        '''
+
+        """ 
+        피쳐 스케일링(Feature scalining)은 해당 피쳐들의 값을 일정한 수준으로 맞춰주는 것이다.
+        이때 적용되는 스케일링 방법이 표준화(standardization) 와 정규화(normalization)다.
+        
+        1단계: 표준화(공통 척도)를 진행한다.
+            표준화는 정규분포를 데이터의 평균을 0, 분산이 1인 표준정규분포로 만드는 것이다.
+            x = (x - mu) / sigma
+            scale = (x - np.mean(x, axis=0)) / np.std(x, axis=0)
+        2단계: 이상치 발견 및 제거
+        3단계: 정규화(공통 간격)를 진행한다.
+            정규화에는 평균 정규화, 최소-최대 정규화, 분위수 정규화가 있다.
+             * 최소최대 정규화는 모든 데이터를 최대값을 1, 최솟값을 0으로 만드는 것이다.
+            도메인은 데이터의 범위이다.
+            스케일은 데이터의 분포이다.
+            목적은 도메인을 일치시키거나 스케일을 유사하게 만든다.              
+        """
+
+        '''x_scaled = min_max_scalar.fit_transform(x.astype(float))
+        police_norm = pd.DataFrame(x_scaled, columns=self.crime_columns, index=police.index)
+        police_norm[self.crime_rate_columns] = police[self.crime_rate_columns]
+        police_norm['범죄'] = np.sum(police_norm[self.crime_rate_columns], axis=1)
+        police_norm['검거'] = np.sum(police_norm[self.crime_columns], axis=1)
+        police_norm.to_csv('./saved_data/police_norm.csv', sep=',', encoding='UTF-8')'''
 
     def folium_test(self):
         #self.file.fname = 'us-states'
